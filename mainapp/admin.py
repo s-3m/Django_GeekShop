a65a6ba639c6b1563febd4d8735c1931a4c5dfb7
s-3m <80763326+s-3m@users.noreply.github.com>
2021-12-16 14:51:05 +0300

    Revert "Lesson3"

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from django.contrib.admin.decorators import register
-from .models import Product, ProductCategory
-# Register your models here.
 
-admin.site.register(Product)
-admin.site.register(ProductCategory)
+# Register your models here.
