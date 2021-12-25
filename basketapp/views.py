707b3588d6e722a4680ef6ffc848f01f31dad9e6
Roman <s-3m@yandex.ru>
2021-12-25 14:38:15 +0300

    Готово ДЗ 6

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from django.urls import reverse
+
 from basketapp.models import Basket
 from mainapp.models import Product
+from django.contrib.auth.decorators import login_required
 import pdb
 
 
+@login_required
 def basket(request):
     title = 'Корзина'
     basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
@@ -11,7 +15,11 @@ def basket(request):
     return render(request, 'basketapp/basket.html', content)
 
 
+@login_required
 def basket_add(request, pk):
+
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:product', args=[pk]))
     product = get_object_or_404(Product, pk=pk)
 
     basket = Basket.objects.filter(user=request.user, product=product).first()
@@ -25,6 +33,7 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, pk):
     basket_record = get_object_or_404(Basket, pk=pk)
     basket_record.delete()
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -126,3 +126,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'authapp.ShopUser'
+
+LOGIN_URL = '/auth/login/'
